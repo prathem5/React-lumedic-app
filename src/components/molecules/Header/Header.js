b58e217dff8a214c637b262c6a389475fed084ed
prathem5 <ppsangle05@gmail.com>
2021-02-15 11:09:24 +0530

    Added states list in form

--- src/components/molecules/Header/Header.js
+++ src/components/molecules/Header/Header.js
@@ -10,13 +10,15 @@ import {
   
 } from 'carbon-components-react';
 import { Link } from 'react-router-dom';
+import lumedic from './lumedic.svg'
+import Logo from './Logo';
 
 const TutorialHeader = () => (
   <div className="container">
     <span>
-      <img src="./lumedic.svg" alt="logo"></img>
+    <Logo/>
     </span>
-    <Header style={{backgroundColor:'#f4f4f4'}} aria-label="IBM Platform Name">
+    <Header style={{backgroundColor:'#f4f4f4',border:0}} >
       <HeaderName style ={{color:'#161616'}}href="#" prefix="">
         Lumedic
       </HeaderName>
